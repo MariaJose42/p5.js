f493a343ad3ce5ab62bc85ea50c3248a617a42f3
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2023-02-17 10:37:50 +0100

    Add files via upload

---
+++ sketch-circulo.js
@@ -0,0 +1,37 @@
+//corre solo una vez cuando inicia el programa
+
+function setup(){
+    console.log("setup - frameCount:"+frameCount);
+     createCanvas(200,200);
+     background (125);
+}
+
+//corre continuamente después de la función setup
+function draw(){
+    clear();
+    background(125);
+    console.log("draw - frameCount:"+frameCount);
+    console.log ("height:" + height);
+    console.log ("width:"+width);
+
+    console.log("mouseX:"+ mouseX + ",mouseY:"+ mouseY)
+
+    ellipse(mouseX,mouseY,50)
+
+    if((mouseX<100) && (mouseY<100)){
+        fill("green");
+    }
+    else if((mouseX<200) && (mouseY<100)){
+        fill("red");
+    }
+
+    else if((mouseX<100) && (mouseY<200)){
+        fill("blue");
+    }
+
+    else if((mouseX<200) && (mouseY<200)){
+        fill("yellow");
+    }
+    
+
+}
--- sketch.js
+++ sketch.js
@@ -2,37 +2,52 @@
 
 function setup(){
     console.log("setup - frameCount:"+frameCount);
-     createCanvas(200,200);
+     createCanvas(400,400);
      background (125);
 }
 
 //corre continuamente después de la función setup
 function draw(){
-    clear();
-    background(125);
+
+    clear()
+    background(125)
+
     console.log("draw - frameCount:"+frameCount);
     console.log ("height:" + height);
     console.log ("width:"+width);
 
-    console.log("mouseX:"+ mouseX + ",mouseY:"+ mouseY)
+    //console.log("mouseX:"+ mouseX + ",mouseY:"+ mouseY)
 
+    console.log("keyCode:"+ keyCode)
+
+    switch (keyCode) {
+        //derecha
+        case 39:
+            textSize(60)
+            text("Derecha",30,30,200,200)
+            break;
+
+        case 37:
+            textSize(60)
+            text("Izquierda",30,30,200,200)
+            break;
+        //Arriba
+        case 38:
+            //fill(225,0,0)
+            //rect(30,30,200,200)
+
+            //fill(225,225,0)
+            textSize(60)
+            text("Arriba",30,30,200,200)
+            //fill(0,0,225)
+            //ellipse (30,30,100)
+            break;
+
+        case 40:
+            textSize(60)
+            text("Abajo",30,30,200,200)
+            break;
     
-    ellipse(mouseX,mouseY,50)
-
-    if((mouseX<100) && (mouseY<100)){
-        fill("green");
     }
-    else if((mouseX<200) && (mouseY<100)){
-        fill("red");
-    }
-
-    else if((mouseX<100) && (mouseY<200)){
-        fill("blue");
-    }
-
-    else if((mouseX<200) && (mouseY<200)){
-        fill("yellow")
-    }
-    
 
 }
