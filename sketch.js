292f479fc9cdbd6f7463a0d25cdc7f405d507d55
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2023-02-17 10:48:44 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 //corre solo una vez cuando inicia el programa
+let x=200 ;
+let y=200;
 
 function setup(){
     console.log("setup - frameCount:"+frameCount);
-     createCanvas(400,400);
+     createCanvas(800,400);
      background (125);
 }
 
@@ -20,32 +22,33 @@ function draw(){
 
     console.log("keyCode:"+ keyCode)
 
+    
+    ellipse(x,y,50)
+
+
     switch (keyCode) {
         //derecha
         case 39:
-            textSize(60)
-            text("Derecha",30,30,200,200)
+            x=x+1;
             break;
 
         case 37:
-            textSize(60)
-            text("Izquierda",30,30,200,200)
+            x=x-1
             break;
         //Arriba
         case 38:
-            //fill(225,0,0)
-            //rect(30,30,200,200)
-
-            //fill(225,225,0)
+            y=y-1
+            /*fill(225,0,0)
+            rect(30,30,200,200)
+            fill(225,225,0)
             textSize(60)
             text("Arriba",30,30,200,200)
-            //fill(0,0,225)
-            //ellipse (30,30,100)
+            fill(0,0,225)
+            ellipse (30,30,100)*/
             break;
 
         case 40:
-            textSize(60)
-            text("Abajo",30,30,200,200)
+            y=y+1
             break;
     
     }
