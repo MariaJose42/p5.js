3add3d98fb3b24df1acb0920e8df7dc3ec72bfc5
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2023-02-21 10:00:49 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,61 @@
+let burbujas=[];
 //corre solo una vez cuando inicia el programa
-let total=100 ;
-let ini_x=0;
-let ini_y=0;
-
 function setup(){
     console.log("setup - frameCount:"+frameCount);
-     createCanvas(300,300);
-     background (125);
+     createCanvas(800,400);
+
+     let x1=random(width) ;
+     let y1=random(height) ;
+     let radio1= random(20,50);
+
+     let b1 = new Burbuja(x1,y1,radio1);
+     burbujas.push(b1);
 }
 
 //corre continuamente después de la función setup
 function draw(){
 
-    console.log("draw - frameCount:"+frameCount);
+    //console.log("draw - frameCount:"+frameCount);
     console.log ("height:" + height);
     console.log ("width:"+ width);
-   
+    background (125);
+    
+    /**for(let i=0; i<burbujas.length; i++){
+        burbujas[i].mover();
+        burbujas[i].mostrar();
+    }**/
 
+    burbujas.forEach(x=>{
+        x.mover();
+        x.mostrar();
+    })
+
+}
+
+function mousePressed(){
+    console.log("("+mouseX+","+mouseY+")");
+
+    let radio= random(10,50);
+    let b = new Burbuja(mouseX,mouseY,radio);
+    burbujas.push(b)
 }
 
 class Burbuja{
     constructor(x,y,radio){
         this.x=x;
         this.y=y;
+        this.radio=radio
     }
+    mover(){
+        this.x= this.x + random(-5,5) ;
+        this.y= this.y + random(-5,5) ;
+    }
+    mostrar(){
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x,this.y,this.radio*2) 
+
+    }
+
 }
